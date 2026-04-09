8c74c05470c23ea9eeb11cf2626f0d3f42ce097d
sara faltas <gadly@cloudconceptgroup.com>
2026-04-09 14:55:01 +0200

    resolved

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const DURATION = 10; // 10 seconds
 let remainingTime = DURATION; // Countdown starting from 10
 let timer = null; // Variable to store the interval
+const countDown = document.querySelector("#time")
 
 
 
 // ITERATION 1: Add event listener to the start button
 
 // Your code goes here ...
-
+const buttonStart = document.querySelector("#start-btn")
+buttonStart.addEventListener("click",startCountdown)
 
 
 
@@ -17,6 +19,17 @@ function startCountdown() {
 
 
   // Your code goes here ...
+  const intervalID = setInterval(() => {
+     buttonStart.disabled = true ;
+  remainingTime-- ;
+  countDown.textContent = remainingTime;
+  if (remainingTime === 0){
+  clearInterval(intervalID);
+  showToast();
+  }
+  },1000)
+  
+ //! why we make text content not innertext?
 }
 
 
@@ -27,6 +40,11 @@ function showToast(message) {
   console.log("showToast called!");
 
   // Your code goes here ...
+  const showToast = document.querySelector("#toast");
+  showToast.classList.add("show")
+  setTimeout (() => {
+  showToast.classList.remove("show")
+  }, 3000)
 
 
 
